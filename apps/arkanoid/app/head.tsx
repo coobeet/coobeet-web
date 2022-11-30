c80780d9ae41af980d174b792bd82f637bc4cbc9
coobeet <coobeet@gmail.com>
2022-11-30 23:55:26 +0800

    feat: setup arkanoid app

---
+++ apps/arkanoid/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <head>
+      <meta charSet={'utf-8'} />
+      <meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
+      <title>Arkanoid</title>
+      <link rel={'icon'} href={'/favicon.ico'} />
+    </head>
+  );
+}
